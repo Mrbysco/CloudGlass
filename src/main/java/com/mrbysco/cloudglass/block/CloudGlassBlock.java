c8390dc13a28a4181e26c3ba6122a093dba8054d
Mrbysco <Mrbysco@users.noreply.github.com>
2024-12-21 21:51:07 +0100

    Stop cloud glass being broken by flowing water

--- src/main/java/com/mrbysco/cloudglass/block/CloudGlassBlock.java
+++ src/main/java/com/mrbysco/cloudglass/block/CloudGlassBlock.java
@@ -2,12 +2,21 @@ package com.mrbysco.cloudglass.block;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BeaconBeamBlock;
+import net.minecraft.world.level.block.LiquidBlockContainer;
 import net.minecraft.world.level.block.TransparentBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import org.jetbrains.annotations.Nullable;
 
-public class CloudGlassBlock extends TransparentBlock implements BeaconBeamBlock {
+public class CloudGlassBlock extends TransparentBlock implements BeaconBeamBlock, LiquidBlockContainer {
 	public static final MapCodec<CloudGlassBlock> CODEC = RecordCodecBuilder.mapCodec(
 			instance -> instance.group(DyeColor.CODEC.fieldOf("color").forGetter(CloudGlassBlock::getColor), propertiesCodec())
 					.apply(instance, CloudGlassBlock::new)
@@ -27,5 +36,15 @@ public class CloudGlassBlock extends TransparentBlock implements BeaconBeamBlock
 	@Override
 	public DyeColor getColor() {
 		return this.color;
+	}
+
+	@Override
+	public boolean canPlaceLiquid(@Nullable Player player, BlockGetter level, BlockPos pos, BlockState state, Fluid fluid) {
+		return false;
+	}
+
+	@Override
+	public boolean placeLiquid(LevelAccessor level, BlockPos pos, BlockState state, FluidState fluidState) {
+		return false;
 	}
 }
